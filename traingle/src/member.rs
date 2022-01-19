252f74980e19664e31bdfc2f3850f069aab2d2ec
Mickey Burks <brks.mck@gmail.com>
2022-01-19 09:38:14 -0600

    Add mutation rate

--- traingle/src/member.rs
+++ traingle/src/member.rs
@@ -7,11 +7,10 @@ use rand_distr::StandardNormal;
 pub struct Member {
     pub source: MemberType,
     point: Box<Point>,
-    mutations: Vec<Member>,
+    mutations: Vec<Option<Member>>,
     size: usize,
     dimensions: (f32, f32),
     pub fitness: f32,
-    //pub is_beneficial: bool,
 }
 
 impl Member {
@@ -28,30 +27,36 @@ impl Member {
             size: 0,
             dimensions,
             fitness: 0.0,
-            //is_beneficial: false,
         }
     }
-    pub fn mutation(&self, index: usize) -> &Member {
+    pub fn mutation(&self, index: usize) -> &Option<Member> {
         if index == 0 {
-            &self
+            return &None;
         } else {
             if index > self.size {
                 panic!("size out of bounds");
             }
-            &self.mutations[index - 1]
+            return &self.mutations[index - 1];
         }
     }
     pub fn point(&self, index: usize) -> &Point {
-        &self.mutation(index).point
+        match self.mutation(index) {
+            Some(m) => &m.point,
+            None => &self.point,
+        }
     }
     pub fn mutate(&mut self) -> () {
         // will have to mess with normal distribution here
-        let random_point = Point::new(random(), random());
-        self.mutations.push(Member::new(
-            MemberType::Mutation(random_point),
-            self.point.values(),
-            self.dimensions,
-        ));
+        if should_mutate() {
+            let random_point = Point::new(random(), random());
+            self.mutations.push(Option::Some(Member::new(
+                MemberType::Mutation(random_point),
+                self.point.values(),
+                self.dimensions,
+            )));
+        } else {
+            self.mutations.push(Option::None);
+        }
         self.size += 1;
     }
     pub fn merge_mutations_into_base(&mut self) -> () {
@@ -60,27 +65,30 @@ impl Member {
         let mut highest = 0.0;
         let base_fitness = self.fitness;
         for mutation in &self.mutations {
-            match mutation.source {
-                MemberType::Mutation(delta) => {
-                    if base_fitness < mutation.fitness {
-                        beneficial_mutations.push((delta, mutation.fitness));
-                        if mutation.fitness > highest {
-                            highest = mutation.fitness;
+            match mutation {
+                Some(m) => match m.source {
+                    MemberType::Mutation(delta) => {
+                        if base_fitness < m.fitness {
+                            beneficial_mutations.push((delta, m.fitness));
+                            if m.fitness > highest {
+                                highest = m.fitness;
+                            }
                         }
                     }
-                }
-                _ => (),
+                    _ => (),
+                },
+                None => (),
             }
         }
         for (delta, fitness) in beneficial_mutations {
             let percent = fitness / highest;
             aggregate.mutate(delta * percent, self.dimensions);
         }
-        self.mutations.push(Member::new(
+        self.mutations.push(Some(Member::new(
             MemberType::Base,
             aggregate.values(),
             self.dimensions,
-        ));
+        )));
         self.size += 1;
     }
     pub fn add_fitness(&mut self, index: usize, fitness: f32) -> () {
@@ -90,7 +98,10 @@ impl Member {
             if index > self.size {
                 panic!("size out of bounds");
             }
-            self.mutations[index - 1].fitness += fitness;
+            match &mut self.mutations[index - 1] {
+                Some(m) => m.fitness += fitness,
+                None => (),
+            }
         }
     }
     fn _add_fitness(&mut self, fitness: f32) -> () {
@@ -100,9 +111,14 @@ impl Member {
         let mut index = 0;
         let mut highest = self.fitness;
         for (i, mutation) in (&self.mutations).into_iter().enumerate() {
-            if mutation.fitness > highest {
-                index = i;
-                highest = mutation.fitness;
+            match mutation {
+                Some(m) => {
+                    if m.fitness > highest {
+                        index = i;
+                        highest = m.fitness;
+                    }
+                }
+                None => (),
             }
         }
         self.values(index)
@@ -128,6 +144,10 @@ impl Clone for MemberType {
 }
 
 const MAX_DEV: f32 = 3.0;
+
+fn should_mutate() -> bool {
+    thread_rng().gen_bool(1.0 / 3.0)
+}
 
 fn random() -> f32 {
     let val: f32 = thread_rng().sample(StandardNormal);
