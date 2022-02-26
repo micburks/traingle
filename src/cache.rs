d7be35d7417013a75c74a46059dd1505038044eb
Mickey Burks <brks.mck@gmail.com>
2022-02-26 10:13:06 -0600

    Add cache module

---
+++ src/cache.rs
@@ -0,0 +1,33 @@
+use super::point::Point;
+
+use std::collections::HashMap;
+
+pub struct Cache {
+    store: HashMap<String, (f32, image::Rgb<u8>)>,
+}
+
+impl Cache {
+    pub fn new() -> Cache {
+        Cache {
+            store: HashMap::new(),
+        }
+    }
+    pub fn insert<F>(
+        &mut self,
+        p1: Point,
+        p2: Point,
+        p3: Point,
+        mut f: F,
+    ) -> (f32, image::Rgb<u8>)
+    where
+        F: FnMut(Point, Point, Point) -> (f32, image::Rgb<u8>),
+    {
+        *self
+            .store
+            .entry(Self::hash(p1, p2, p3))
+            .or_insert(f(p1, p2, p3))
+    }
+    fn hash(p1: Point, p2: Point, p3: Point) -> String {
+        format!("{},{}-{},{}-{},{}", p1.0, p1.1, p2.0, p2.1, p3.0, p3.1)
+    }
+}
